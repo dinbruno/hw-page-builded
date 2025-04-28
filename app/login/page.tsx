9fcfc2d94c5e1711b2506545eba716d688b11050
dinbruno <brunodinosantos@Outlook.com>
2025-04-28 10:36:58 -0300

    Add metadata for LoginPage to enhance SEO and user context
    - Introduced metadata definitions for the LoginPage, including title and description for improved SEO.
    - Configured Open Graph properties to enhance social media sharing and visibility.
    
    These changes collectively improve the application's search engine optimization and user engagement during the login process.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,17 @@
 import { useLoginPage } from "./components/login.model";
 import { LoginView } from "./components/login.view";
 import LoginLoadingAdvanced from "./components/login.loading";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Faça login para acessar sua conta e gerenciar suas páginas.",
+  openGraph: {
+    title: "Login",
+    description: "Faça login para acessar sua conta e gerenciar suas páginas.",
+    type: "website",
+  },
+};
 
 export default function LoginPage() {
   const loginProps = useLoginPage();
